1c9325ea5e62d12ca0b0bf47ac56948c1dd65f26
Darren Cal <darrencal64@gmail.com>
2022-02-06 15:08:18 -0600

    first commit

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBbP9MtjMU4GJCI2aUt6mTFRYA_OwLYTuY",
+    authDomain: "dc-demos-65426.firebaseapp.com",
+    projectId: "dc-demos-65426",
+    storageBucket: "dc-demos-65426.appspot.com",
+    messagingSenderId: "790501091392",
+    appId: "1:790501091392:web:3378bb024be7232fa317a5",
+    measurementId: "G-Z403MZ6N22"
+};
