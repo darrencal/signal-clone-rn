8cdc63244803c2869e3615cf6809392fa904eebd
Darren Cal <darrencal64@gmail.com>
2022-02-11 20:20:49 -0600

    implemented chat screen with messages

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import { collection, doc, onSnapshot, orderBy, query } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Avatar, ListItem } from 'react-native-elements';
+import { db } from '../firebase';
 
 const CustomListItem = ({ id, chatName, enterChat }) => {
-  return (
-    <ListItem onPress={() => enterChat(id, chatName)} key={id} bottomDivider>
-        <Avatar
-            rounded
-            source={{
-                uri: 'https://connectingcouples.us/wp-content/uploads/2019/07/avatar-placeholder.png',
-            }}
-        />
-        <ListItem.Content>
-            <ListItem.Title style={{ fontWeight: '800' }}>{chatName}</ListItem.Title>
-            <ListItem.Subtitle numberOfLines={1} ellipsizeMode='tail'>
-                This is a test subtitle
-            </ListItem.Subtitle>
-        </ListItem.Content>
-    </ListItem>
-  );
+    const [chatMessages, setChatMessages] = useState([]);
+
+    useEffect(() => {
+        const unsubscribe = onSnapshot(
+            query(
+                collection(doc(collection(db, 'signal_chats'), id), 'messages'), 
+                orderBy('timestamp', 'desc')
+            ), (snapshot) => setChatMessages(snapshot.docs.map(doc => doc.data()))
+        );
+
+        return unsubscribe;
+    }, []);
+
+    return (
+        <ListItem onPress={() => enterChat(id, chatName)} key={id} bottomDivider>
+            <Avatar
+                rounded
+                source={{
+                    uri: chatMessages[0]?.photoURL ||
+                        'https://connectingcouples.us/wp-content/uploads/2019/07/avatar-placeholder.png',
+                }}
+            />
+            <ListItem.Content>
+                <ListItem.Title style={{ fontWeight: '800' }}>{chatName}</ListItem.Title>
+                <ListItem.Subtitle numberOfLines={1} ellipsizeMode='tail'>
+                    {chatMessages[0] ? `${chatMessages[0].displayName}: ${chatMessages[0].message}` : ''}
+                </ListItem.Subtitle>
+            </ListItem.Content>
+        </ListItem>
+    );
 };
 
 export default CustomListItem;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -25,7 +25,8 @@ const HomeScreen = ({ navigation }) => {
                     id: doc.id,
                     data: doc.data()
                 }))
-        ));
+            )
+        );
 
         return unsubscribe;        
     }, []);
@@ -70,11 +71,23 @@ const HomeScreen = ({ navigation }) => {
 
     }, [navigation]);
 
+    const enterChat = (id, chatName) => {
+        navigation.navigate('Chat', {
+            id,
+            chatName
+        });
+    }
+
     return (
         <SafeAreaView>
             <ScrollView style={styles.container}>
                 {chats.map(({id, data: { chatName }}) => (
-                    <CustomListItem key={id} id={id} chatName={chatName} />
+                    <CustomListItem
+                        key={id}
+                        id={id}
+                        chatName={chatName}
+                        enterChat={enterChat}
+                    />
                 ))}
             </ScrollView>
         </SafeAreaView>
